358ad051f107c3bf34ade40d6b30ec8f521a1a07
gloryarz <gloryarz@gmail.com>
2018-08-09 20:00:59 -0500

    Empezando maquetado

--- src/js/ultima.js
+++ src/js/ultima.js
@@ -1,6 +1,9 @@
 const workerName = document.getElementById('workerName');
+const visitorName = document.querySelector('#visitorName');
 let worker = localStorage.getItem('worker');
+let visitor = localStorage.getItem('userName');
 
+visitorName.innerHTML = visitor;
 workerName.innerHTML = worker;
 
 setTimeout((event) => {
