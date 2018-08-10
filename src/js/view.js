465241592698b470727262c9dd19fa26d38ca4d6
María Fernanda Benítez Rivera <39814132+FeerRivera@users.noreply.github.com>
2018-08-09 21:18:04 -0500

    cambios de gloria y regreso de carpetas

--- src/js/view.js
+++ src/js/view.js
@@ -2,23 +2,29 @@ const userName = document.getElementById('user-Name');
 const userVisit = document.getElementById('user-visit');
 const bttnContinue = document.getElementById('continue');
 
+let nameWork = 'Lau';
+let mailTrial = 'lau@gmail.com';
+
+localStorage.setItem('worker', nameWork);
+localStorage.setItem('mail', mailTrial);
+
 bttnContinue.addEventListener('click', (e) => {
- e.preventDefault(e);
- let newUser = userName.value;
+  e.preventDefault(e);
+  let newUser = userName.value;
+  localStorage.setItem('userName', newUser);
+  window.location.href = '/src/views/foto.html';
 
-
- if (form.checkValidity() === true) {
-   let fbRef = fb.collection('user').add({
-     name: userNameValue,
-
-   }).then(function(docRef) {
-     console.log('Document written with ID: ', docRef.id);
-   })
-     .catch(function(error) {
-       console.error('Error', error);
-     });
-       window.location.href = '../views/foto.html';
- } else {
-   form.reportValidity();
- }
+  if (form.checkValidity() === true) {
+    let fbRef = fb.collection('user').add({
+      name: userNameValue,
+    }).then(function(docRef) {
+      console.log('Document written with ID: ', docRef.id);
+    })
+      .catch(function(error) {
+        console.error('Error', error);
+      });
+    window.location.href = 'views/foto.html';
+  } else {
+    form.reportValidity();
+  }
 });
